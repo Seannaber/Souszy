20b38550bb7474b38de7b7d25ac1cc3e2dca1b8c
Sean Naber <seannaber@gmail.com>
2017-01-07 18:29:08 -0500

    Working sliding menu added

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var burgertime = function() {
+	$("#menu").css({left:0});
+	$("#thedarkness").css({display: "inline"});
+}
+
+var byebye = function() {
+	$("#menu").css({left:"-300px"});
+	$("#thedarkness").css({display: "none"});
+}
