2421724bc3a18566e88a1835d1bf05cdeb466584
Sean Naber <seannaber@gmail.com>
2017-01-07 18:56:37 -0500

    tweaking css and js for menu

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 var burgertime = function() {
 	$("#menu").css({left:0});
-	$("#thedarkness").css({display: "inline"});
+	$("#thedarkness").css({visibility: "visible"});
 }
 
 var byebye = function() {
-	$("#menu").css({left:"-300px"});
-	$("#thedarkness").css({display: "none"});
+	$("#menu").css({left:"-1500px"},{transition: "all 300ms"});
+	$("#thedarkness").css({visibility: "hidden"});
 }
