51856b5b0be1393f9f94df3de9cd16ce2da487d7
Sean Naber <seannaber@gmail.com>
2017-01-14 16:06:29 -0500

    prev & next buttons working

--- js/script.js
+++ js/script.js
@@ -9,5 +9,28 @@ var byebye = function() {
 };
 
 var grilledcheese = function() {
+	$.cookie("Menu1", 0);
+	$("#grilledcheese" + Number($.cookie("Menu1"))).css({display: "block"});
+	$("main").css({visibility: "hidden"});
+};
 
+var grilledCheeseNext = function() {
+	if ($.cookie("Menu1") == 4) {
+		$.cookie("Menu1", 0);
+		$("#grilledcheese1").css({display: "none"});
+		$("#grilledcheese2").css({display: "none"});
+		$("#grilledcheese3").css({display: "none"});
+		$("#grilledcheese4").css({display: "none"});
+		$("#appcontainer").css({visibility: "visible"});
+	} else {
+		$("#grilledcheese" + Number($.cookie("Menu1"))).css({display: "none"});
+		$.cookie("Menu1", (Number($.cookie("Menu1")) + 1));
+		$("#grilledcheese" + Number($.cookie("Menu1"))).css({display: "block"});
+	};
+}
+
+var grilledCheesePrev = function() {
+	$("#grilledcheese" + Number($.cookie("Menu1"))).css({display: "none"});
+	$.cookie("Menu1", (Number($.cookie("Menu1")) - 1));
+	$("#grilledcheese" + Number($.cookie("Menu1"))).css({display: "block"});
 };
